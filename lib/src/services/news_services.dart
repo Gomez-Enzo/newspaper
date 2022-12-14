6d81e93987edda7bfcf1ebed1f5e4dfd9e275469
Gomez Enzo <gomezenzo1128@gmail.com>
2022-12-14 11:23:35 -0300

    Get news by categories

--- lib/src/services/news_services.dart
+++ lib/src/services/news_services.dart
@@ -9,6 +9,7 @@ final _apiKey = '431a2b39236d43baaafbf67530faa12d';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+  String _selectedCategory = 'business';
   List<Category> categories = [
     Category(FontAwesomeIcons.building, 'business'),
     Category(FontAwesomeIcons.tv, 'entertainment'),
@@ -18,14 +19,37 @@ class NewsService with ChangeNotifier {
     Category(FontAwesomeIcons.volleyball, 'sport'),
     Category(FontAwesomeIcons.memory, 'technology'),
   ];
+  Map<String, List<Article>> categoryArticles = {};
   NewsService() {
     getTopHeadlines();
+    categories.forEach((item) {
+      categoryArticles[item.name] = [];
+    });
   }
+  String get selectedCategory => _selectedCategory;
+  set selectedCategory(String valor) {
+    _selectedCategory = valor;
+    getArticleByCategory(valor);
+    notifyListeners();
+  }
+
   getTopHeadlines() async {
     final url = Uri.parse('$_urlNews/top-headlines?apiKey=$_apiKey&country=us');
     final resp = await http.get(url);
     final newsResponse = newsResponseFromJson(resp.body);
     headlines.addAll(newsResponse.articles);
+    notifyListeners();
+  }
+
+  getArticleByCategory(String category) async {
+    if (categoryArticles[category]!.isNotEmpty) {
+      return categoryArticles[category];
+    }
+    final url = Uri.parse(
+        '$_urlNews/top-headlines?apiKey=$_apiKey&country=us&category=$category');
+    final resp = await http.get(url);
+    final newsResponse = newsResponseFromJson(resp.body);
+    categoryArticles[category]!.addAll(newsResponse.articles);
     notifyListeners();
   }
 }
